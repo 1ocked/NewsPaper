a1fa410be798e251f3e9156ed7e9b512f0522f2d
Ruslan <habay@mail.ru>
2024-10-15 03:19:54 +0400

    Переписаны коды

--- news/views.py
+++ news/views.py
@@ -2,9 +2,10 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 
-class PostList(ListView):
+class PostList(ListView):                                      ### Есть видео от ментора!
     # Указываем модель, объекты которой мы будем выводить
     model = Post
     # Поле, по которому будет выполняться сортировка
@@ -18,6 +19,7 @@ class NewsDetails(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Post
     # Используем другой шаблон — product.html
-    template_name = 'news.html'
+    template_name = 'new.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'Post'
+
